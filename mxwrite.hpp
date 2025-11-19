50017a49b7f945351e6ce0e64ad010e6c8d9f2b1
Jared Bruni <jaredbruni@gmail.com>
2025-11-18 20:39:57 -0800

    Refactor Writer class and add new methods

--- mxwrite.hpp
+++ mxwrite.hpp
@@ -25,13 +25,14 @@ public:
 
     Writer() : opened(false), queue_mutex(), writer_mutex(), frame_mutex() {}
 
-    bool open(const std::string& filename, int width, int height, float fps, int bitrate_kbps);
+    bool open(const std::string& filename, int width, int height, float fps, const char *crf);
     void write(void* rgba_buffer);
-    bool open_ts(const std::string& filename, int width, int height, float fps, int bitrate_kbps);
+    bool open_ts(const std::string& filename, int width, int height, float fps, const char *crf);
     void write_ts(void* rgba_buffer);
     void close();
     bool is_open() const { return opened; }
-  
+    int64_t get_frame_count() const { return frame_count; } 
+    double get_duration() const;
     ~Writer() {
         if (is_open()) {
             close();
@@ -43,7 +44,10 @@ private:
     bool opened {false};
     int width = 0;
     int height = 0;
-    int frame_count = 0;
+    int fps_num = 0;
+    int fps_den = 0;
+    int64_t frame_count = 0;
+    double last_duration = 0.0; 
     AVFormatContext* format_ctx = nullptr;
     AVCodecContext* codec_ctx = nullptr;
     AVStream* stream = nullptr;
@@ -53,8 +57,6 @@ private:
     AVRational time_base;
     void calculateFPSFraction(float fps, int &fps_num, int &fps_den);
     std::chrono::steady_clock::time_point recordingStart;
-    int fps_num = 0; 
-    int fps_den = 0; 
     std::queue<Frame_Data> frame_queue;
     const size_t MAX_QUEUE_SIZE = 30; 
     std::mutex queue_mutex{};
@@ -65,4 +67,4 @@ private:
 extern void transfer_audio(std::string_view sourceAudioFile, std::string_view destVideoFile);
 extern void cleanup_contexts(AVFormatContext* source_ctx, AVFormatContext* dest_ctx, AVFormatContext* output_ctx);
 
-#endif 
+#endif
